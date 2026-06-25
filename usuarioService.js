8513eba0ebb0716a9225e92207acb282992acd9a
Zeldrismilgrau <Gabrielbrfernandes000@gmail.com>
2026-06-24 21:44:00 -0300

    test do joão refeito kkkk

--- usuarioService.js
+++ usuarioService.js
@@ -1,6 +1,6 @@
 function buscarUsuarioNoBanco(id) {
   // Imagine que isso acessa banco/API
-  return { id, nome: 'Maria' };
+  return { id, nome: 'João Mockado' };
 }
 
 module.exports = { buscarUsuarioNoBanco };
